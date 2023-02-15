1e71aeaa4ba2eb5d0328a8d6c45709c1e09165c1
nurnastianingsi <125424722+nurnastianingsi@users.noreply.github.com>
2023-02-14 16:35:20 -1200

    Add files via upload

---
+++ week2.php
@@ -0,0 +1,11 @@
+<?php
+echo "week 2 <hr>";
+
+//variabel
+$nama = $_POST['nama'];
+$jurusan = $_POST['jurusan'];
+$mk = $_POST['mk'];
+$nilai = $_POST['nilai'];
+
+// //echo 'Nama : ' . $nama;
+echo "Nama : $nama <br> Jurusan : $jurusan <br> Mk : $mk <br> Nilai : $nilai";
