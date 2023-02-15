5fd30cb05a33f55f468f87f1872eec01c92ddbea
nurnastianingsi <125424722+nurnastianingsi@users.noreply.github.com>
2023-02-14 16:38:08 -1200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+#general
+define('base_url', 'https://sidatura.com/');
+define('webName', "Sidatura | Badan Penanggulangan Bencana Daerah Kab. SBT");
+
+#DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'sidatura_admin');
+define('DB_PASS', 's^DT8X{kI#wm');
+define('DB_NAME', 'sidatura_db');
+
+//TANGGAL
+define('NOW', date("Y-m-d H:i:s"));
